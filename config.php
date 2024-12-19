72951dc722b85ac155208687570534c9a4f680a1
Kriszel-Catunao <147257052+CatunaoKriszel@users.noreply.github.com>
2024-12-19 09:12:20 +0700

    config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 		$servername = "kriszel";
-		$username = "kriszel"; 
-		$password = "Catunao@01"; 
+		$username = "angeline"; 
+		$password = "Calona@22"; 
 		$dbname = "mybook"; 
 
 		$conn = new mysqli($servername, $username, $password, $dbname);
